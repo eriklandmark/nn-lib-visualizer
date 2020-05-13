51b0333fd4ac08cf93132c86e41557efef64f02a
Erik Landmark <erik@landmark.se>
2020-05-13 15:03:15 +0200

    added build files

---
+++ dist/interface/precache-manifest.be26de0a0bb999983eb87620adc70072.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef5c208abe7f09d3695",
+    "url": "/css/app.0ee964d4.css"
+  },
+  {
+    "revision": "8c151b55a0265cd5c7dc",
+    "url": "/css/chunk-vendors.5c2f0050.css"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "c60a3c1114c8a1675c3867bfc3528a53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef5c208abe7f09d3695",
+    "url": "/js/app.fb31e71c.js"
+  },
+  {
+    "revision": "8c151b55a0265cd5c7dc",
+    "url": "/js/chunk-vendors.8d034c9f.js"
+  },
+  {
+    "revision": "803480defbd9591b8dd9a32e2cadf11f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
